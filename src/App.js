8d2cf0ce0285803a519c335619d4231ac8b90690
Harsh <root@harshsinghvi.com>
2021-04-02 12:09:19 +0530

    ✨ NEW FEATURE: Github Fork Ribbin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
 import TodoApp from './TodoApp';
 import { Provider } from 'react-redux' //Redux Provider
 import store from './redux/store' //Redux Store
 import Footer from './footer'
+import ForkMeOnGithub from 'fork-me-on-github';
 
 function App() {
   return (
@@ -11,6 +11,9 @@ function App() {
 
     <div className="App">
       <h1> Todo App</h1>
+      <ForkMeOnGithub 
+       repo="https://github.com/harshsinghvi/todo-app"
+      />
       <TodoApp />
       <Footer />
     </div>
